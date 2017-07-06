a912e0a130b3fb83307c88c791f49bdb6811ac00
guogh <guogaohu8@163.com>
2017-07-06 16:22:13 +0800

    add shadowsocksr-libev package

--- tmp/info/.files-packageinfo.mk
+++ tmp/info/.files-packageinfo.mk
@@ -1,3 +1,4 @@
 DEPS_package/linux/Makefile=modules/*.mk $(TOPDIR)/target/linux/*/modules.mk
+$(eval $(call PackageDir,custom-package_shadowsocksr-libev,custom-package/shadowsocksr-libev,))
 $(eval $(call PackageDir,linux,linux,))
 $(eval $(call PackageDir,toolchain,toolchain,))
